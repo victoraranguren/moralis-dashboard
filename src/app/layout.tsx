28db930b931185210a3c55bf1e26fb55fa1c4cca
Victor <victoraranguren.dev@gmail.com>
2024-12-11 18:26:13 -0400

    First commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@
 import localFont from "next/font/local";
 import "./globals.css";
 import { UnifiedWalletProvider } from "@jup-ag/wallet-adapter";
+import Moralis from "moralis";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -15,33 +17,42 @@ const geistMono = localFont({
   weight: "100 900",
 });
 
+Moralis.start({
+  apiKey:
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJub25jZSI6IjU0NGQxODU3LTMxOGMtNDNmNS1hNzg1LTM4NTc3ZGNkN2FiNCIsIm9yZ0lkIjoiNDAwOTEwIiwidXNlcklkIjoiNDExOTU1IiwidHlwZUlkIjoiOTcxODZlMTAtYWFjNS00MzdhLTlmZDQtZDRlYWI3MjVhNzQ5IiwidHlwZSI6IlBST0pFQ1QiLCJpYXQiOjE3MjE1OTk2MDEsImV4cCI6NDg3NzM1OTYwMX0.t-dhVOz687FhnEJNTyuVhhWvYThJcazuv346oCdmVLg",
+});
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const queryClient = new QueryClient();
+
   return (
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <UnifiedWalletProvider
-          children={children}
-          wallets={[]}
-          config={{
-            autoConnect: false,
-            env: "mainnet-beta",
-            metadata: {
-              name: "UnifiedWallet",
-              description: "UnifiedWallet",
-              url: "https://jup.ag",
-              iconUrls: ["https://jup.ag/favicon.ico"],
-            },
-            walletlistExplanation: {
-              href: "https://station.jup.ag/docs/additional-topics/wallet-list",
-            },
-          }}
-        />
+        <QueryClientProvider client={queryClient}>
+          <UnifiedWalletProvider
+            children={children}
+            wallets={[]}
+            config={{
+              autoConnect: false,
+              env: "mainnet-beta",
+              metadata: {
+                name: "UnifiedWallet",
+                description: "UnifiedWallet",
+                url: "https://jup.ag",
+                iconUrls: ["https://jup.ag/favicon.ico"],
+              },
+              walletlistExplanation: {
+                href: "https://station.jup.ag/docs/additional-topics/wallet-list",
+              },
+            }}
+          />
+        </QueryClientProvider>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,52 @@
 "use client";
-import { UnifiedWalletButton } from "@jup-ag/wallet-adapter";
+import { UnifiedWalletButton, useWallet } from "@jup-ag/wallet-adapter";
 import { Button } from "@/components/ui/button";
+import { useEffect } from "react";
+import Moralis from "moralis";
+import { useQuery } from "@tanstack/react-query";
 
 export default function Home() {
+  const { connected, publicKey } = useWallet();
+
+  const getPortfolio = async () => {
+    try {
+      if (publicKey) {
+        const portfolio = await Moralis.SolApi.account.getPortfolio({
+          network: "mainnet",
+          address: publicKey.toString(),
+        });
+
+        console.log(portfolio.raw);
+        return portfolio;
+      }
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  const { isPending, error, data } = useQuery({
+    queryKey: ["portfolio", connected],
+    queryFn: getPortfolio,
+  });
+
+  useEffect(() => {
+    console.log(publicKey?.toString());
+  }, [connected]);
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <UnifiedWalletButton />
-      <Button>Get Balance</Button>
+
+      {isPending && "Loading..."}
+
+      {error && "An error has occurred: " + error.message}
+
+      {connected &&
+        "Su balance de lamports es: " + data?.raw.nativeBalance.lamports}
+
+      {!connected && "Su Wallet está desconectada"}
+
+      {data && data.raw.nativeBalance.lamports}
     </div>
   );
 }
